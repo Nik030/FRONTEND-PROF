98a488da8ba5472d21100c2cf8861935a8a5908b
Nikita.Schneider <nikitasch2001@gmail.com>
2023-02-23 10:10:51 +0100

    23.02 new2

--- PROF FRONTEND/JavaScript/forEach/index.js
+++ PROF FRONTEND/JavaScript/forEach/index.js
@@ -1,7 +1,14 @@
-function forEach(arr, callback){}  // функция для каждого элемента вызывает колбэк
+function forEach(arr, callback){
+for (let i = 0; i < arr.length; i++) {
+    callback(i)  
+}
+}  // функция для каждого элемента вызывает колбэк
 
 const array = [1, 2, 3];
 
-forEach(array, function (element) {
-    console.log(element);
-})
+
+
+function printNumber() {
+    console.log(1);
+}
+console.log(printNumber());
