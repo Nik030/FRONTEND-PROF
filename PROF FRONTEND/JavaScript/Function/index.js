95f52c6a2d95e8c82e25b99ddea081147cfb9d25
Nikita.Schneider <nikitasch2001@gmail.com>
2023-02-24 09:52:59 +0100

    .

--- PROF FRONTEND/JavaScript/Function/index.js
+++ PROF FRONTEND/JavaScript/Function/index.js
@@ -1,11 +1,27 @@
 
-
+//1
 console.log(sum(1, 2, 5));
 function sum(a, b, c) {
     return a + b + c;
 }
 
-const fn = function(a) {
+//2
+const fn = function (a) {
     return a ** 2;
 }
 console.log(fn(4));
+
+const anotherFn = fn;
+
+console.log(anotherFn(9));
+
+//3
+function doFiveTimes(callback) {
+    // callback = console.log
+    for (let i = 0; i < 5; i++) {
+        callback(i);
+    }
+}
+doFiveTimes(console.log);
+
+
