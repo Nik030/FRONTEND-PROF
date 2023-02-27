bedcbe43257c574c83e55e72d058bb0c19922d8b
Nikita.Schneider <nikitasch2001@gmail.com>
2023-02-27 13:12:09 +0100

    1

---
+++ PROF FRONTEND/JavaScript/Lesson_3/script.js
@@ -0,0 +1,12 @@
+const arr = [12, 4, -3, -43, 12, -4, 3];
+// вывести только положительные числа
+
+forEach(arr, function (element) {
+    if (arr[i]>0) {
+        console.log(arr[i]);
+        i++
+        
+    }
+    
+    // +=  ===  result + element
+})
