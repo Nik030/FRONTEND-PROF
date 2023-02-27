6be08c34a64a880cd8323980433080f1dc51d18d
Nikita.Schneider <nikitasch2001@gmail.com>
2023-02-27 13:26:45 +0100

    1

--- PROF FRONTEND/JavaScript/Lesson_3/script.js
+++ PROF FRONTEND/JavaScript/Lesson_3/script.js
@@ -14,3 +14,18 @@ arr.forEach(function (item) {
         console.log(item);
     }    
 })
+
+
+// используя forEach вывести в консоль наименования товаров
+const products = [
+    {id: 1, title: 'велосипед', price: 12000, discount: 15},
+    {id: 2, title: 'ролики', price: 700, discount: 25},
+    {id: 3, title: 'самокат', price: 500, discount: 0},
+    {id: 4, title: 'лыжи', price: 1300, discount: 5},
+    {id: 5, title: 'сноуборд', price: 2000, discount: 0},
+    {id: 6, title: 'коньки', price: 1700, discount: 10}
+]
+
+products.id.forEach(function (item) {
+   console.log(title);  
+})
