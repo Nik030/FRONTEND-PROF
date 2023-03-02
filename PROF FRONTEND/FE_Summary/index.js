4c5d9ee35e7452ceb177d03e58ff2f8e84ff96d6
Nikita Schneider <nikitasch2001@gmail.com>
2023-03-02 21:05:16 +0100

    1

--- PROF FRONTEND/FE_Summary/index.js
+++ PROF FRONTEND/FE_Summary/index.js
@@ -1,7 +1,21 @@
 // 24.02
-
+// 1-й способ задания функции
 function sum(a, b, c) { // объявляем функцию именем sum
     return a + b + c;
 }
-
 console.log(sum(1, 2, 5));
+
+
+
+// 2-й
+const fn = function (a) {
+    return a ** 2;
+}
+console.log(fn(4));
+
+
+// 3-й
+function sum(a, b, c = 4) { 
+    return a + b + c;
+}
+console.log(sum(1, 2));
