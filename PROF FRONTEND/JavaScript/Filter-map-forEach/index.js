3d9a523002ca78020873737971a4505f1c2fc98d
Nikita-Schneider <nikitasch2001@gmail.com>
2023-03-09 21:55:55 +0100

    28.02

--- PROF FRONTEND/JavaScript/Filter-map-forEach/index.js
+++ PROF FRONTEND/JavaScript/Filter-map-forEach/index.js
@@ -15,11 +15,21 @@ function map(arr, callback) {
     return newArr;
 }
 
+// фунция фильтр создает массив, переберает его и вкладывает в него те элементы, которые прошли проверку вложенной вторым аргументом функцией
 function filter(arr, callback) {
     let newArr = [];
     forEach(arr, function(el, i, arr) {
         const isFilterPassed = callback(el, i, arr);
-        if (isFilterPassed) newArr.push(newEl);
+        if (isFilterPassed) newArr.push(el);
     });
     return newArr;
 }
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const resultArray = filter(array, function (el) {
+    return el % 2 === 0;
+});
+console.log(resultArray);
+// 2, 4, 6, 8
+// почему-то не работает
