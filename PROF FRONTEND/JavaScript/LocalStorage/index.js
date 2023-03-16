5e44d8b4ff1bd97e9186b47f7da99e67c86b62f8
Nikita-Schneider <nikitasch2001@gmail.com>
2023-03-16 23:26:28 +0100

    111

--- PROF FRONTEND/JavaScript/LocalStorage/index.js
+++ PROF FRONTEND/JavaScript/LocalStorage/index.js
@@ -2,10 +2,23 @@
 
 
 
+
 const button = document.querySelector("input[type='button']");
 const newToDoInput = document.querySelector("input[type='text']");
+const list = document.querySelector("#list")
+const newToDoContainer = document.createElement("label");
+const newToDoValue = document.createElement("input");
+const paragraph = document.createElement("p");
 
-const handleAdd = event => console.log(newToDoInput.value) // выводим listener  в отдельную переменную
 
+newToDoValue.setAttribute("type", "checkbox");
+paragraph.innerText = "Some text";
+newToDoContainer.append(newToDoValue, paragraph);
+list.append(newToDoContainer);
+
+const handleAdd = event => {
+ console.log(newToDoInput.value) // выводим listener  в отдельную переменную
+ newToDoInput.value = ""
+}
 button.addEventListener("click", handleAdd) // вешаем обработчик события на кнопку
 
