1623c728685aad8909426700c2fde23e5befe8b3
Nikita-Schneider <nikitasch2001@gmail.com>
2023-03-20 21:57:56 +0100

    111

--- PROF FRONTEND/JavaScript/LocalStorage/index.js
+++ PROF FRONTEND/JavaScript/LocalStorage/index.js
@@ -3,20 +3,22 @@ const newToDoInput = document.querySelector("input[type='text']");
 const list = document.querySelector("#list");
 
 
-const createToDo = () => {
+const createToDo = (text) => {
   const newToDoContainer = document.createElement("label");
   const newToDoValue = document.createElement("input");
   const paragraph = document.createElement("p");
 
   newToDoValue.setAttribute("type", "checkbox");
-  paragraph.innerText = "Some text";
+  paragraph.innerText = text;
   newToDoContainer.append(newToDoValue, paragraph);
   return newToDoContainer;
 };
 
-list.append(createToDo());
+list.append(createToDo("Some text 1"));
+list.append(createToDo("Some text 2"));
+list.append(createToDo("Some text 3"));
 
-const handleAdd = (event) => {
+const handleAdd = event => {
   console.log(newToDoInput.value); // выводим listener  в отдельную переменную
   newToDoInput.value = "";
 };
